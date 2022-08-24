6bce4c979e6ef6006d07ca6619481f10182a8917
Steven Green <ste2144105@gmail.com>
2022-08-24 10:45:40 -0700

    setup questions

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+var questions = [
+    {question: "What are arrays wrapped in?", choices: ["()", "[]", "{}", "''"], answer: "[]"},
+     
+    {question: "What is an example of an arithmetic operator?", choices: ["<", ">", "+", "==="], answer: "+"},
+
+    {question: "What word does a for loop start with?", choices: ["var", "if", "let", "for"], answer: "for"},
+
+    {question: "What symbols would you use to increment?", choices: ["++", "--", "==", ">>"], answer: "++"},
+
+    {question: "How can you style an element in javascript?", choices: ["for loop", "set attribute", "if statement", "bubbling"], answer: "set attribute"},
+
+    {question: "What number does an index start with?", choices: ["0", "1", "2", "3"], answer: "0"},
+];
