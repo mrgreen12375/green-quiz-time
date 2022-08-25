27a6274ba9cafaae744d51f8c3939468f657915c
Steven Green <ste2144105@gmail.com>
2022-08-25 16:18:54 -0700

    setup fuctions to power the quiz

--- javascript.js
+++ javascript.js
@@ -46,3 +46,106 @@ mainSection.appendChild(begin);
 mainSection.appendChild(answerLi);
 mainSection.appendChild(solution);
 
+function startQuestions(){
+
+    clearScreen();
+    
+    if (questionList.length == 0){
+        gameOver();
+    }else{
+        
+        pushQuestion = questionList.pop();
+        question.textContent = pushQuestion.question;
+        askedQuestions.push(pushQuestion);
+        
+        for(let i=0; i < pushQuestion.choices.length; i++){
+            answer[i] = document.createElement('li');
+            answer[i].textContent = pushQuestion.choices[i];
+            answer[i].addEventListener('click', isRight);
+            answerLi.appendChild(answer[i]); 
+        }
+    } 
+}
+
+function isRight(event){
+
+    if(pushQuestion.answer == event.target.textContent){
+        currentScore++;
+        solution.textContent = "Correct!";
+    }else{
+        solution.textContent = "Wrong!";
+        timeLeft -= 10;
+    }
+    startQuestions();
+}
+
+function beginTimer() {
+    timeInterval = setInterval(function() {
+      timeLeft--;
+      if(timeLeft < 0){
+        timeLeft = 0;
+    }
+      time.textContent = "Time: " + timeLeft;
+  
+      if(timeLeft <= 0) {
+        gameOver();
+      }
+    }, 1000);
+}
+
+function clearScreen(){
+    while(answerLi.hasChildNodes()) {
+        answerLi.removeChild(answerLi.firstChild);
+        question.textContent = '';
+    }
+}
+
+function gameOver(){
+    clearInterval(timeInterval);
+
+    clearScreen();
+    solution.textContent = '';
+    
+    if (questionList.length == 0){
+        question.textContent = "You answered "+ currentScore + " out of 6";
+    }else{
+        question.textContent = "You answered "+ currentScore + " out of 6";
+    }
+
+    userName = document.createElement('input');
+    userName.setAttribute('type', 'text');
+    userName.setAttribute('placeholder', 'Initials Here')
+    solution.appendChild(userName);
+
+    logScore = document.createElement('button');
+    logScore.textContent = "Enter High Score";
+    mainSection.appendChild(logScore);
+
+    logScore.addEventListener('click', function(){
+
+        var user = {
+            name: "N/A",
+            score: currentScore,
+        }
+
+        newname = userName.value.trim();
+        if(newname){
+            user.name = newname;
+        }
+
+        var storedUsers = JSON.parse(localStorage.getItem("userScores"))
+        if (storedUsers !== null) {
+            userScores = storedUsers;
+            console.log(userScores);
+        }
+        userScores.push(user);
+        localStorage.setItem("userScores", JSON.stringify(userScores));
+        window.location.href = 'scores.html';
+    })
+}
+
+begin.addEventListener("click", function(){
+    beginTimer();
+    startQuestions();
+    begin.remove();
+});
