ca560896a876941b84b05e41a1385a43399bbc2c
Steven Green <ste2144105@gmail.com>
2022-08-25 22:42:43 -0700

    add notes to score.js

--- scores.js
+++ scores.js
@@ -1,13 +1,14 @@
+//created variables to select the list and store the users scores.
 leaderBoard = document.querySelector('ul');
 var userScores = [];
 
-//Loads saved local data
+//created variable to load the scores in local storage. 
 var storedUsers = JSON.parse(localStorage.getItem("userScores"))
     if (storedUsers !== null) {
         userScores = storedUsers;
 }
 
-//Creates list of items for parsed storedUser
+//Created for loop to create the list of users stored scores. 
 for(let i=0; i < userScores.length; i++){
     var userScore = document.createElement('li');
     userScore.innerHTML = userScores[i].name + "<span> SCORE: "+userScores[i].score+"</span>";
