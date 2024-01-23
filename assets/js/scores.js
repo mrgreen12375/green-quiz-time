6ab35bcc94fd2c455a6dd5a15304fbba1dae957a
Steven Green <ste2144105@gmail.com>
2024-01-22 22:09:09 -0700

    setup if statement for highscore

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,11 +5,14 @@ var clear = document.querySelector('#clear');
 //created variable to load the scores in local storage. 
 var userScores = JSON.parse(localStorage.getItem("userScores"));
 
-//Created for loop to create the list of users stored scores. 
-for(let i=0; i < userScores.length; i++){
-    var userScore = document.createElement('li');
-    userScore.innerHTML = userScores[i].name + "<span> SCORE: " + userScores[i].score + "</span>";
-    leaderBoard.appendChild(userScore);
+if (userScores === null) {
+    console.log("No Highscores Yet!")
+} else {
+    for(let i=0; i < userScores.length; i++){
+        var userScore = document.createElement('li');
+        userScore.innerHTML = userScores[i].name + "<span> SCORE: " + userScores[i].score + "</span>";
+        leaderBoard.appendChild(userScore);
+    }
 }
 
 function clearHighScores() {
